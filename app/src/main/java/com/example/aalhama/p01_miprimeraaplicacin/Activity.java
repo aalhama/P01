ab4693b1f936fc2a99d00adf0273572a0a53a63f
Adrian Alhama Carrasco <aalhama@elpuig.xeill.net>
2016-09-28 18:58:24 +0200

    Initial commit

---
+++ app/src/main/java/com/example/aalhama/p01_miprimeraaplicacin/Activity.java
@@ -0,0 +1,13 @@
+package com.example.aalhama.p01_miprimeraaplicacin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity);
+    }
+}
